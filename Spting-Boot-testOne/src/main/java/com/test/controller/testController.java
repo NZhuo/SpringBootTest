8860e9e9161e31e49d2c0d24850be8b872176d88
NZhuo <1533504974@qq.com>
2022-04-28 16:43:08 +0800

    hello world

---
+++ Spting-Boot-testOne/src/main/java/com/test/controller/testController.java
@@ -0,0 +1,26 @@
+package com.test.controller;
+
+import com.test.domain.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class testController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello nanz";
+    }
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
